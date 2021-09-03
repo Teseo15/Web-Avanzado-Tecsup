cd68313dc02afcf6bebb44ddfafe14a405a7ae84
Brayan  Blas <brayan.blas.egoavil30@gmail.com>
2021-09-02 19:16:04 -0500

    add crud history

--- blogTech/backend/components/history/controller.js
+++ blogTech/backend/components/history/controller.js
@@ -1,47 +1,50 @@
 import { nanoid } from "nanoid";
 import { response,reque } from "../../../network";
 import { list, find, remove } from "../../../store/dummy";
-/**
- * * CREATE_STRORY => user_id, title, author, text, dateTime
- * * READ_STORY => story_id
- * * UPDATE_STORY => story_id, story_data
- * * DELETE_STORY => story_id
- */
-//*POST
-const USER_TABLE = "stories";
+
+const history_TABLE = "stories";
 export const create = async (req,res)=>{
     const history = req.body
-
   //* Creamos el data de la historia nueva
   const data = {
     id: nanoid(),
     title: history.title,
     content: history.content,
-    user_id: history.user_id,
+    history_id: history.history_id,
     author: history.author,
   };
 
-  const historys = await store(USER_TABLE, data);
+  const historys = await store(history_TABLE, data);
 
   return response({ res, data: historys });
 };
+
 export const read = async (req,res)=>{
     const { id } = req.params;
 
-    const history = await find(USER_TABLE, id);
+    const history = await find(history_TABLE, id);
     return response({ res, data: history});
 };
-export const update = (req,res)=> {
-    const { title,author,text} = req.body;
-    return response({
-    res,    
-    data: { title,author,text},
-    });
+export const up = async (req, res) => {
+  // recibe los datos
+  const history = req.body;
+  const { id } = req.params;
+
+  const data = {
+    id: id,
+    title: history.title,
+    content: history.content,
+    history_id: history.history_id,
+    author: history.author,
+  };
+  const uphistory = await update(history_TABLE, id,data);
+  return response({ res, data: uphistory });
+  
 };
 export const destroy = async (req,res)=>{ 
     const { id } = req.params;
 
-    const history = await remove(USER_TABLE, id);
+    const history = await remove(history_TABLE, id);
   
     if (!history) {
       return response({ res, ok: false, data: { error: "History not found" } });
